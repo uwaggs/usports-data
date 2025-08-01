be43c27df60adcc49145c7e6a67fa026a76f1659
Chowtown <blux2703@gmail.com>
2025-08-01 15:22:15 -0400

    Debugging pretend computer

--- update/test.r
+++ update/test.r
@@ -1,4 +1,13 @@
-pacman::p_load(rvest)
+pacman::p_load(rvest, httr)
 
-read_html("https://en.usports.ca/sports/mbkb/2024-25c/boxscores/20250316_ta5h.xml") |>
-  html_table()
+url <- "https://en.usports.ca/sports/mbkb/2024-25c/boxscores/20250316_ta5h.xml"
+
+res <- GET(url, user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"))
+
+if (status_code(res) == 200) {
+  page <- read_html(res)
+  tables <- html_table(page)
+  print(tables)
+} else {
+  stop("Request failed with status: ", status_code(res))
+}
