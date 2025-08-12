9d1c2f46c3bc1ef47d996fad9c2d8f8c0372de3b
Chowtown <blux2703@gmail.com>
2025-08-12 15:58:30 -0400

    remove old function

--- backfill/initialize_schedule.R
+++ backfill/initialize_schedule.R
@@ -1,48 +1,3 @@
-# library(usportsscraper)
-# library(rvest)
-# library(tidyverse)
-# library(piggyback)
-#
-# # Function to initialize the schedule
-#
-# initialize_schedule <- function() {
-#
-#   # Get year
-#   current_year <- as.integer(format(Sys.Date(), "%Y"))
-#   current_year_abbreviated <- substr(current_year, 3, 4)
-#   sports <- c("mvball", "wvball", "mice", "mbkb", "wbkb", "msoc", "wsoc", "wice", "fh", "fball")
-#
-#   for(sport in sports) {
-#     print(sport)
-#
-#     # find available seasons
-#     seasons <- paste0("https://en.usports.ca/sports/",sport,"/",
-#                     current_year-1,"-",
-#                     current_year_abbreviated,
-#                     "/schedule") %>%
-#       read_html() %>%
-#       html_elements(".form-control.season-filter.form-select") %>%
-#       html_children() %>%
-#       html_text2()
-#
-#     # scrape all schedules
-#     schedule <- lapply(seasons, function(season) usportsscraper::scrape_schedule(sport = sport, season = season)) %>%
-#       bind_cols()
-#
-#     write_csv(schedule, paste(sport, "schedules.csv", sep = "_"))
-#
-#     # upload to releases
-#     piggyback::pb_upload(
-#       file = paste(sport, "schedules.csv", sep = "_"),
-#       repo = "uwaggs/usports-data",
-#       tag = "schedules",
-#       overwrite = TRUE
-#     )
-#
-#     sys.sleep(10) # Sleep to avoid hitting the server too hard
-#   }
-# }
-
 initialize_schedule <- function() {
   years <- 2009:(lubridate::year(Sys.Date()) - 1)
   seasons <- paste0(years, "-", substr(years + 1, 3, 4))
@@ -55,7 +10,7 @@ initialize_schedule <- function() {
     for(season in seasons) {
       teams <- usportsscraper::team_names(league, season)
       variants <- usportsscraper::full_season(season)
-
+      print(season)
       for(team in teams) {
         Sys.sleep(6)
         for(variant in variants) {
