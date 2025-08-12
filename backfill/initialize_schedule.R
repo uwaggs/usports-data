cb1274a3a36201d9f08828bbbc86fa8307010907
Chowtown <blux2703@gmail.com>
2025-08-12 15:46:33 -0400

    Updated loops

--- backfill/initialize_schedule.R
+++ backfill/initialize_schedule.R
@@ -51,28 +51,36 @@ initialize_schedule <- function() {
 
   all_schedule <- data.frame()
 
-  for (league in leagues) {
+  for(league in leagues) {
     for(season in seasons) {
-      schedule <- usportsscraper::scrape_schedule(league, season)
-      all_schedule <- dplyr::bind_rows(all_schedule, schedule)
-    }
-    all_schedule |>
-      dplyr::mutate(path = stringr::str_glue("data/{league}_schedule/{league}_schedule_{season}.csv")) |>
-      dplyr::group_by(season) |>
-      dplyr::group_split() |>
-      purrr::walk( ~ {
-        fs::dir_create(dirname(.x$path[1]))
-        readr::write_csv(.x, .x$path[1])
-      })
+      teams <- usportsscraper::team_names(league, season)
+      variants <- usportsscraper::full_season(season)
 
-    sapply(
-      unique(all_schedule$season), \(x)
-      piggyback::pb_upload(
-        file = glue::glue("data/{league}_schedule/{league}_schedule_{x}.csv"),
-        repo = "uwaggs/usports-data",
-        tag = paste0(league, "_schedule"),
-        overwrite = TRUE
-      )
-    )
+      for(team in teams) {
+        Sys.sleep(6)
+        for(variant in variants) {
+          schedule <- usportsscraper::scrape_game_log_safe(league, season, team)
+          all_schedule <- dplyr::bind_rows(all_schedule, schedule)
+        }
+        all_schedule |>
+          dplyr::mutate(path = stringr::str_glue("data/{league}_schedule/{league}_schedule_{season}.csv")) |>
+          dplyr::group_by(season) |>
+          dplyr::group_split() |>
+          purrr::walk( ~ {
+            fs::dir_create(dirname(.x$path[1]))
+            readr::write_csv(.x, .x$path[1])
+          })
+
+        sapply(
+          unique(all_schedule$season), \(x)
+          piggyback::pb_upload(
+            file = glue::glue("data/{league}_schedule/{league}_schedule_{x}.csv"),
+            repo = "uwaggs/usports-data",
+            tag = paste0(league, "_schedule"),
+            overwrite = TRUE
+          )
+        )
+      }
+    }
   }
 }
