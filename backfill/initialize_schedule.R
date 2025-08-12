4833bc46f9fe9062def808382c5861cf9fcc1fff
Chowtown <blux2703@gmail.com>
2025-08-12 11:02:49 -0400

    Initialize Schedule

--- backfill/initialize_schedule.R
+++ backfill/initialize_schedule.R
@@ -1,52 +1,51 @@
-library(usportsscraper)
-library(rvest)
-library(tidyverse)
-library(piggyback)
-
-# Function to initialize the schedule
+# library(usportsscraper)
+# library(rvest)
+# library(tidyverse)
+# library(piggyback)
+#
+# # Function to initialize the schedule
+#
+# initialize_schedule <- function() {
+#
+#   # Get year
+#   current_year <- as.integer(format(Sys.Date(), "%Y"))
+#   current_year_abbreviated <- substr(current_year, 3, 4)
+#   sports <- c("mvball", "wvball", "mice", "mbkb", "wbkb", "msoc", "wsoc", "wice", "fh", "fball")
+#
+#   for(sport in sports) {
+#     print(sport)
+#
+#     # find available seasons
+#     seasons <- paste0("https://en.usports.ca/sports/",sport,"/",
+#                     current_year-1,"-",
+#                     current_year_abbreviated,
+#                     "/schedule") %>%
+#       read_html() %>%
+#       html_elements(".form-control.season-filter.form-select") %>%
+#       html_children() %>%
+#       html_text2()
+#
+#     # scrape all schedules
+#     schedule <- lapply(seasons, function(season) usportsscraper::scrape_schedule(sport = sport, season = season)) %>%
+#       bind_cols()
+#
+#     write_csv(schedule, paste(sport, "schedules.csv", sep = "_"))
+#
+#     # upload to releases
+#     piggyback::pb_upload(
+#       file = paste(sport, "schedules.csv", sep = "_"),
+#       repo = "uwaggs/usports-data",
+#       tag = "schedules",
+#       overwrite = TRUE
+#     )
+#
+#     sys.sleep(10) # Sleep to avoid hitting the server too hard
+#   }
+# }
 
 initialize_schedule <- function() {
-
-  # Get year
-  current_year <- as.integer(format(Sys.Date(), "%Y"))
-  current_year_abbreviated <- substr(current_year, 3, 4)
-  sports <- c("mvball", "wvball", "mice", "mbkb", "wbkb", "msoc", "wsoc", "wice", "fh", "fball")
-
-  for(sport in sports) {
-    print(sport)
-
-    # find available seasons
-    seasons <- paste0("https://en.usports.ca/sports/",sport,"/",
-                    current_year-1,"-",
-                    current_year_abbreviated,
-                    "/schedule") %>%
-      read_html() %>%
-      html_elements(".form-control.season-filter.form-select") %>%
-      html_children() %>%
-      html_text2()
-
-    # scrape all schedules
-    schedule <- lapply(seasons, function(season) usportsscraper::scrape_schedule(sport = sport, season = season)) %>%
-      bind_cols()
-
-    write_csv(schedule, paste(sport, "schedules.csv", sep = "_"))
-
-    # upload to releases
-    piggyback::pb_upload(
-      file = paste(sport, "schedules.csv", sep = "_"),
-      repo = "uwaggs/usports-data",
-      tag = "schedules",
-      overwrite = TRUE
-    )
-
-    sys.sleep(10) # Sleep to avoid hitting the server too hard
-  }
-}
-
-initialize_schedule <- function() {
-  seasons <- c("2009-10", "2010-11", "2011-12", "2012-13", "2013-14",
-               "2014-15", "2015-16", "2016-17", "2017-18", "2018-19",
-               "2019-20", "2020-21", "2021-22", "2022-23", "2023-24")
+  years <- 2009:(lubridate::year(Sys.Date()) - 1)
+  seasons <- paste0(years, "-", substr(years + 1, 3, 4))
 
   leagues <- c("mbkb", "wbkb", "fh", "fball", "msoc", "wsoc", "mice", "wice", "mvball", "wvball")
 
