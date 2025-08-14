8d61f4009afb6e224cca288b3efe776598195dee
Chowtown <blux2703@gmail.com>
2025-08-14 12:02:43 -0400

    Initialize_schedule updated

--- backfill/initialize_schedule.R
+++ backfill/initialize_schedule.R
@@ -2,40 +2,34 @@ initialize_schedule <- function() {
   years <- 2009:(lubridate::year(Sys.Date()) - 1)
   seasons <- paste0(years, "-", substr(years + 1, 3, 4))
 
-  leagues <- c("mbkb", "wbkb", "fh", "fball", "msoc", "wsoc", "mice", "wice", "mvball", "wvball")
-
-  all_schedule <- data.frame()
+  leagues <- c("mbkb", "wbkb", "fh", "mice", "wice", "msoc", "wsoc", "mvball", "wvball", "fball")
 
   for(league in leagues) {
+    all_schedule <- data.frame()
+    print(league)
     for(season in seasons) {
-      teams <- usportsscraper::team_names(league, season)
+      teams <- usportsscraper::team_names_safe(league, season)
       variants <- usportsscraper::full_season(season)
       print(season)
       for(team in teams) {
         Sys.sleep(6)
         for(variant in variants) {
-          schedule <- usportsscraper::scrape_game_log_safe(league, season, team)
+          schedule <- usportsscraper::scrape_game_log_safe(league, variant, team)
           all_schedule <- dplyr::bind_rows(all_schedule, schedule)
         }
-        all_schedule |>
-          dplyr::mutate(path = stringr::str_glue("data/{league}_schedule/{league}_schedule_{season}.csv")) |>
-          dplyr::group_by(season) |>
-          dplyr::group_split() |>
-          purrr::walk( ~ {
-            fs::dir_create(dirname(.x$path[1]))
-            readr::write_csv(.x, .x$path[1])
-          })
-
-        sapply(
-          unique(all_schedule$season), \(x)
-          piggyback::pb_upload(
-            file = glue::glue("data/{league}_schedule/{league}_schedule_{x}.csv"),
-            repo = "uwaggs/usports-data",
-            tag = paste0(league, "_schedule"),
-            overwrite = TRUE
-          )
-        )
       }
+      closeAllConnections()
+      print(nrow(all_schedule))
     }
+    all_schedule <- dplyr::distinct(all_schedule, home_team, away_team, date, .keep_all = TRUE)
+
+    readr::write_csv(all_schedule, glue::glue("data/schedules/{league}_schedule.csv"))
+
+    piggyback::pb_upload(
+      file = glue::glue("data/schedules/{league}_schedule.csv"),
+      repo = "uwaggs/usports-data",
+      tag = paste0("schedules"),
+      overwrite = TRUE
+    )
   }
 }
